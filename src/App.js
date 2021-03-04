5a2bdb9999c5276b051bdb4d68e44dd56f6da0c2
SIMLAM <Simon.Lambrix@essers.com>
2021-03-04 22:21:04 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+		<hr />
+		<p>Hi, i'm elfo</p>
         <a
           className="App-link"
           href="https://reactjs.org"
